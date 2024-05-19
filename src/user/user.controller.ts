3c16432beb631035075fb1a53031c7563a994787
Felipe <amorimfelipe.lfra@gmail.com>
2024-05-19 09:54:56 -0300

    implemented update by put/patch and delete user routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
@@ -15,5 +15,26 @@ export class UserController {
     @Get(':id')
     public async getUserById(@Param() params: {id: string}): Promise<any> {
         return {user: {}, params};
+    }
+
+    @Put(':id')
+    public async updateUser(@Param() params: {id: string}, @Body() body: any): Promise<any> {
+        return {
+            body,
+            params,
+        };
+    }
+
+    @Patch(':id')
+    public async updatePartialUser(@Param() params: {id: string}, @Body() body: any): Promise<any> {
+        return {
+            body,
+            params,
+        };
+    }
+
+    @Delete(':id')
+    public async deleteUser(@Param() params: {id: string}): Promise<any> {
+        return params;
     }
 }
