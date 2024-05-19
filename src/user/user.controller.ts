f58ec54dc633a4b60e40af4009cdaafd29f7060d
Felipe <amorimfelipe.lfra@gmail.com>
2024-05-19 09:48:12 -0300

    added method to list all users and search an user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
     @Post()
     public async createUser(@Body() body: any): Promise<any> {
         return body;
+    }
+
+    @Get()
+    public async getUser(): Promise<any> {
+        return {users: []};
+    }
+
+    @Get(':id')
+    public async getUserById(@Param() params: {id: string}): Promise<any> {
+        return {user: {}, params};
     }
 }
